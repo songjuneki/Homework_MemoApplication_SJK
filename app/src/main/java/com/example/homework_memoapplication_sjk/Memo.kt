1f0ed3eaa2de03adb77c4f94b71c0404fa9d3922
songjuneki <dev.sjk0105@gmail.com>
2024-02-08 19:30:23 +0900

    refactor: LocalDateTime 타입 프로퍼티를 Strin 타입으로 변경

--- app/src/main/java/com/example/homework_memoapplication_sjk/Memo.kt
+++ app/src/main/java/com/example/homework_memoapplication_sjk/Memo.kt
@@ -2,11 +2,10 @@ package com.example.homework_memoapplication_sjk
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
-import java.time.LocalDateTime
 
 @Parcelize
 data class Memo(
     val title: String,
     val content: String,
-    val date: LocalDateTime
+    val date: String
 ): Parcelable
