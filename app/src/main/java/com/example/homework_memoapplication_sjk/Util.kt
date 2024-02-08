00419f94a41cf0422b14b3ca4dde9344d8e7fffe
songjuneki <dev.sjk0105@gmail.com>
2024-02-08 19:28:52 +0900

    remove: LocalDateTime 문자열 변환 메서드 삭제 feat: 키보드를 올리고 내리는 메서드 추가

--- app/src/main/java/com/example/homework_memoapplication_sjk/Util.kt
+++ app/src/main/java/com/example/homework_memoapplication_sjk/Util.kt
@@ -1,17 +1,34 @@
 package com.example.homework_memoapplication_sjk
 
-import java.text.SimpleDateFormat
-import java.time.LocalDateTime
-import java.util.Locale
+import android.content.Context
+import android.os.SystemClock
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.concurrent.thread
 
 class Util {
-    companion object {
 
-        // LocalDateTime 객체를 한글로 출력하는 메서드
-        // yyyy.MM.dd HH:mm:ss 형식으로 출력한다.
-        // 예) 1998.01.05 12:30:59
-        fun dateString(date: LocalDateTime): String {
-            return SimpleDateFormat("yyyy.MM.dd HH:mm:ss", Locale.KOREA).format(date)
+    companion object {
+        // 포커스를 주고 키보드를 올려주는 메서드
+        fun showSoftKey(view: View, context: Context) {
+            // 포커스를 준다
+            view.requestFocus()
+
+            thread {
+                SystemClock.sleep(1000)
+                val inputMethodManager = context.getSystemService(AppCompatActivity.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputMethodManager.showSoftInput(view, 0)
+            }
+        }
+
+        // 키보드를 내려준다
+        fun hideSoftInput(activity: AppCompatActivity) {
+            // 현재 포커스를 가지고 있는 view 가 있다면 키보드를 내린다
+            if (activity.window.currentFocus != null) {
+                val inputMethodManager = activity.getSystemService(AppCompatActivity.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputMethodManager.hideSoftInputFromWindow(activity.window.currentFocus?.windowToken, 0)
+            }
         }
     }
 }
