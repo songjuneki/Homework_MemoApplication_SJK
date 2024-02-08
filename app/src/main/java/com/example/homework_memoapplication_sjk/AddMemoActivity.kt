8d92a8b167319676cfc83eca6b84ab0d4ae611f8
songjuneki <dev.sjk0105@gmail.com>
2024-02-08 17:06:31 +0900

    feat: 메모 추가 Activity 클래스 추가

---
+++ app/src/main/java/com/example/homework_memoapplication_sjk/AddMemoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.homework_memoapplication_sjk
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddMemoActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
