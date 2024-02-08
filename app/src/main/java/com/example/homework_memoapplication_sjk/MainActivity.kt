7019b73e5a33f9bfe47f71bef59e3ace48a2df66
songjuneki <dev.sjk0105@gmail.com>
2024-02-08 19:32:34 +0900

    refactor: Memo 모델의 프로퍼티 타입 변경으로 인한 메모 날짜 TextView 설정 변경

--- app/src/main/java/com/example/homework_memoapplication_sjk/MainActivity.kt
+++ app/src/main/java/com/example/homework_memoapplication_sjk/MainActivity.kt
@@ -93,7 +93,7 @@ class MainActivity : AppCompatActivity() {
         override fun onBindViewHolder(holder: MemoViewHolder, position: Int) {
             holder.binding.apply {
                 memoRawTitle.text = memoList[position].title
-                memoRawDate.text = Util.dateString(memoList[position].date)
+                memoRawDate.text = memoList[position].date
             }
         }
     }
