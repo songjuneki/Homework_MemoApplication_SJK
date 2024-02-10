3cbc6c189d0e64a259c9d0c9ec71f74906010d9b
songjuneki <dev.sjk0105@gmail.com>
2024-02-10 15:01:15 +0900

    feat: 주석 추가 feat: 메모 정보 및 수정 Activity 관련 기능 구현

--- app/src/main/java/com/example/homework_memoapplication_sjk/MainActivity.kt
+++ app/src/main/java/com/example/homework_memoapplication_sjk/MainActivity.kt
@@ -17,10 +17,19 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    // 메모 추가 Activity 관련 프로퍼티
     private lateinit var addMemoContract: ActivityResultContracts.StartActivityForResult
     private lateinit var addMemoLauncher: ActivityResultLauncher<Intent>
 
+    // 메모 보기 및 숮어 Activity 관련 프로퍼티
+    private lateinit var infoMemoContract: ActivityResultContracts.StartActivityForResult
+    private lateinit var infoMemoLauncher: ActivityResultLauncher<Intent>
+
+    // 실제 보여질 메모 List
     private val memoList: MutableList<Memo> = mutableListOf()
+
+    // 메모 수정할 때 사용할 인덱스 번호
+    private var selectedMemoPosition: Int = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +37,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         initAddMemoContract()
+        initInfoMemoContract()
         initToolbar()
         initRecyclerView()
     }
@@ -43,6 +53,31 @@ class MainActivity : AppCompatActivity() {
                     it.data?.getParcelableExtra("memo")
 
                 addMemo(memo)
+            }
+        }
+    }
+
+    // 메모 보기 및 수정 Activity Launcher 를 설정한다.
+    private fun initInfoMemoContract() {
+        infoMemoContract = ActivityResultContracts.StartActivityForResult()
+        infoMemoLauncher = registerForActivityResult(infoMemoContract) {
+            if (it.resultCode == RESULT_OK) {
+                val isDeleted = it.data?.getBooleanExtra("deleted", false) ?: false
+                if (isDeleted) {
+                    memoList.removeAt(selectedMemoPosition)
+                    binding.homeMemoList.adapter?.notifyItemRemoved(selectedMemoPosition)
+                    return@registerForActivityResult
+                }
+
+                val modifiedMemo: Memo? = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+                    it.data?.getParcelableExtra("memo", Memo::class.java)
+                else
+                    it.data?.getParcelableExtra("memo")
+
+                modifiedMemo?.let { after ->
+                    memoList[selectedMemoPosition] = after
+                    binding.homeMemoList.adapter?.notifyItemChanged(selectedMemoPosition)
+                }
             }
         }
     }
@@ -79,11 +114,22 @@ class MainActivity : AppCompatActivity() {
         binding.homeMemoList.adapter?.notifyDataSetChanged()
     }
 
-    inner class MemoRecyclerAdapter: RecyclerView.Adapter<MemoRecyclerAdapter.MemoViewHolder>() {
-        inner class MemoViewHolder(val binding: MemoRawBinding): RecyclerView.ViewHolder(binding.root) {
+    inner class MemoRecyclerAdapter : RecyclerView.Adapter<MemoRecyclerAdapter.MemoViewHolder>() {
+        inner class MemoViewHolder(val binding: MemoRawBinding) :
+            RecyclerView.ViewHolder(binding.root) {
             init {
-                binding.root.layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+                binding.root.layoutParams =
+                    LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+
+                binding.memoRawCard.setOnClickListener {
+                    selectedMemoPosition = adapterPosition
+                    val intent = Intent(this@MainActivity, InfoMemoActivity::class.java).apply {
+                        putExtra("memo", memoList[adapterPosition])
+                    }
+                    infoMemoLauncher.launch(intent)
+                }
             }
+
         }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MemoViewHolder {
